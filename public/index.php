4a06ff5f31c1902e91f194fec1a2f6811699fd3e
Sultan Syarif Ubaidillah Al Nashr <sultansyarif630@gmail.com>
2025-12-09 19:12:20 +0700

    Halaman Publik

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+$page = $_GET['page'] ?? 'home';
+include 'layout/header.php';
+include 'layout/navbar.php';
+
+if (file_exists("pages/$page.php")) {
+    include "pages/$page.php";
+} else {
+    echo "<h1>404 - Page Not Found</h1>";
+}
+
+include 'layout/footer.php';
+?>
