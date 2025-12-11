fba808868a36b5525847cd0eb810f16b2ee335b9
Yulia Nadzifah <ilhansalihilsa@gmail.com>
2025-12-11 08:03:14 +0700

    produk frontend

--- public/pages/product.php
+++ public/pages/product.php
@@ -1,3 +1,23 @@
+<div class="inner-banner py-5">
+        <section class="w3l-breadcrumb text-left py-sm-5 ">
+            <div class="container">
+                <div class="w3breadcrumb-gids">
+                    <div class="w3breadcrumb-left text-left">
+                        <h2 class="inner-w3-title mt-sm-5 mt-4">
+                            Our Products </h2>
+                    </div>
+                    <div class="w3breadcrumb-right">
+                        <ul class="breadcrumbs-custom-path">
+                            <li><a href="index.php">Home</a></li>
+                            <li class="active"><span class="fas fa-angle-double-right mx-2"></span> Product</li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
+
 <section class="py-5">
     <div class="container py-md-5 py-3">
         <h3 class="title-w3l mb-4">Our Products</h3>
