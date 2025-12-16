6718cde90d3ffadee6e38b31c1a3d39a2ef2e125
Yulia Nadzifah <ilhansalihilsa@gmail.com>
2025-12-16 12:33:42 +0700

    hilangin darkmode

--- public/layout/navbar.php
+++ public/layout/navbar.php
@@ -78,10 +78,6 @@
                     <div class="theme-switch-wrapper">
                         <label class="theme-switch" for="checkbox">
                             <input type="checkbox" id="checkbox">
-                            <div class="mode-container">
-                                <i class="gg-sun"></i>
-                                <i class="gg-moon"></i>
-                            </div>
                         </label>
                     </div>
                 </nav>
